599d0d15a9276c43f5360ecbefdbe4a6b5963d3b
harmful-chan <1148706823@qq.com>
2019-04-18 20:07:24 +0800

    写了啥忘了...

--- WpfResumeBrowsingSystem.WebApi/Controllers/FilesController.cs
+++ WpfResumeBrowsingSystem.WebApi/Controllers/FilesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,24 +16,33 @@ using Microsoft.Extensions.Primitives;
 
 namespace WpfResumeBrowsingSystem.WebApi.Controllers
 {
-    [Consumes("multipart/form-data")]//此处为新增
+    /// <summary>
+    /// 文件上传下载控制器
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class FilesController : ControllerBase
     {
         private readonly IHostingEnvironment _hostingEnvironment;
 
+        /// <summary>
+        /// 构造函数初始化属性IHostingEnvironment
+        /// </summary>
+        /// <param name="hostingEnvironment"></param>
         public FilesController(IHostingEnvironment hostingEnvironment)
         {
             this._hostingEnvironment = hostingEnvironment;
         }
 
-        //POST api/files
+
         /// <summary>
-        /// 上传文件接口，获取POST请求的内容文件，filename+当前时间+.jpg保存进images文件夹中
+        /// POST请求 上传文件接口 保存文件名为:filename+当前时间单位ms+.jpg 路径/wwwroot/images
         /// </summary>
-        /// <param name="form">请求体</param>
-        /// <returns>最后更新文件名</returns>
+        /// <param name="form">请求表单</param>
+        /// <returns>更新文件表单
+        ///     文件：状态
+        /// </returns>
+        //POST api/files
         [HttpPost]
         public IActionResult Post(IFormCollection form)
         {
@@ -47,21 +57,20 @@ namespace WpfResumeBrowsingSystem.WebApi.Controllers
             {
                 try
                 {
-
-
                     //添加当时间进文件名,保存文件
                     string currentFileName = Path.Combine(wwwDirectory, "images",
-                        file.Name + "_" + DateTime.Now.ToString("yyyyMMddhhmmss") + "." + file.FileName.Split('.').Last());
-                    updateFileName.Add(currentFileName);
+                        file.Name + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + "." + file.FileName.Split('.').Last());
+                    
                     using (FileStream fs = System.IO.File.Create(currentFileName))
                     {
                         file.CopyTo(fs);
                         fs.Flush();
                     }
+                    updateFileName.Add(currentFileName);
                 }
-                finally
+                catch(Exception ex)
                 {
-
+                    Console.WriteLine(ex.Message);
                 }
             }
 
@@ -75,48 +84,79 @@ namespace WpfResumeBrowsingSystem.WebApi.Controllers
             
         }
 
+        /// <summary>
+        /// GET请求，显示文件列表
+        /// </summary>
+        /// <param name="id">主键
+        ///     0:含有文件名的所有文件
+        ///     1:含有文件名.扩展名的所有文件
+        /// </param>
+        /// <param name="fileName">文件名.扩展名</param>
+        /// <returns>文件表单</returns>
         //GET api/files?filename
-        [HttpGet("{id}")]
-        public IActionResult Get(int id, string fileName)
+        [HttpGet]
+        public IActionResult Get(int id, string fileName = null)
         {
-            DirectoryInfo dir = new DirectoryInfo(Path.Combine(this._hostingEnvironment.WebRootPath, "images"));
-            List<FileInfo> result = FindFile(dir, fileName);
+            if (fileName == null) return NotFound("Error:FileName Is Null");
 
-            if (0 == id) return Ok(result.ConvertAll<string>(x => x.Name));
-            else if (1 == id)
+            List<FileInfo> fileInfos = FindFile(
+                new DirectoryInfo(Path.Combine(this._hostingEnvironment.WebRootPath, "images")), fileName.Split('.')[0]);
+            if (fileInfos.Count <= 0) return NotFound("Error:File Not Found");
+
+            IEnumerable<FileInfo> fileExtenSames = fileInfos.Where(f=>f.Name.Contains(Path.GetExtension(fileName)));    //匹配扩展名相通的文件
+            return Ok(fileExtenSames.ToList().ConvertAll<string>(f=>f.Name));    //返回所有匹配的文件名
+        }
+
+        /// <summary>
+        /// GET请求，下载最新文件
+        /// </summary>
+        /// <param name="fileName">文件名(可不包含系统添加的时间信息).扩展名</param>
+        /// <returns>文件相应</returns>
+        [HttpGet("download")]
+        public IActionResult Download(string fileName = null)
+        {
+
+            if (fileName == null) return null;
+
+            if (!System.IO.File.Exists(Path.Combine(this._hostingEnvironment.WebRootPath, "images", fileName)))   //文件不存在
             {
-                List<DateTime> fileUpdateDate =    //更新时间
-                    result.ConvertAll<string>(x => x.Name)
-                    .ConvertAll<DateTime>(s => {
-                        string[] tmp = s.Split('.', '_');
-                        return DateTime.ParseExact(
-                            tmp[tmp.Length - 2],
-                            "yyyyMMddhhmmss",
-                            System.Globalization.CultureInfo.CurrentCulture);
-                    });
-                Func<List<DateTime>, int> FindMaxDateIndex = x => {
+                List<FileInfo> fileInfos = FindFile(
+                new DirectoryInfo(Path.Combine(this._hostingEnvironment.WebRootPath, "images")), fileName.Split('.')[0]);
+                if (fileInfos.Count <= 0) return null;
+
+                //匹配扩展名相通的文件
+                List<FileInfo> fileExtenSames = fileInfos.Where(f => f.Name.Contains(Path.GetExtension(fileName))).ToList();
+
+                //获取文件更新时间
+                List<DateTime> fileUpdateDates = GetFileUpdateDateList(fileExtenSames);
+
+                //最新文件位置
+                Func<List<DateTime>, int> IndexOfNewest = x =>
+                {
                     int maxIndex = -1;
                     for (int i = 0; i < x.Count - 1; i++)
                         for (int j = i; j < x.Count - 1; j++)
                             maxIndex = (DateTime.Compare(x[i], x[i + 1]) > 0) ? i : i + 1;
                     return maxIndex;
                 };
-                int index = FindMaxDateIndex(fileUpdateDate);
-                using (FileStream fs = System.IO.File.OpenRead(result[index].FullName))
-                {
-                    string fileExt = Path.GetExtension(result[index].FullName);
-                    var provider = new FileExtensionContentTypeProvider();
-                    var memi = provider.Mappings[fileExt];
-                    return File(fs, memi, Path.GetFileName(result[index].FullName));
-                }
+                int index = IndexOfNewest(fileUpdateDates);
+
             }
-            else
-            {
-                return NotFound();
-            }
+
+            //响应
+            FileStream fs = System.IO.File.OpenRead(
+                Path.Combine(this._hostingEnvironment.WebRootPath, "images", fileExtenSames[index].Name));
+            var fileExt = Path.GetExtension(fileExtenSames[index].Name);
+            var memi = new FileExtensionContentTypeProvider().Mappings[fileExt];
+            return File(fs, memi, Path.GetFileName(fileExtenSames[index].Name));
         }
 
-        //查找文件
+        /// <summary>
+        /// 指定目录查找文件名含有字符片段的文件
+        /// </summary>
+        /// <param name="dirs">指定目录</param>
+        /// <param name="name">字符片段</param>
+        /// <returns>文件信息列表</returns>
         private List<FileInfo> FindFile(DirectoryInfo dirs, string name)
         {
             List<FileInfo> result = new List<FileInfo>();
@@ -129,6 +169,27 @@ namespace WpfResumeBrowsingSystem.WebApi.Controllers
                 if (file.Name.IndexOf(name) >= 0) result.Add(file);
             }
             return result;
+        }
+        
+        /// <summary>
+        /// 从文件名获取文件更新时间
+        /// </summary>
+        /// <param name="fileInfo">文件信息</param>
+        /// <returns>时间</returns>
+        private DateTime GetFileUpdateDate(FileInfo fileInfo)
+        {
+            string[] tmp = fileInfo.Name.Split('.', '_');
+            return DateTime.ParseExact(tmp[tmp.Length - 2], "yyyyMMddhhmmss", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// GetFileUpdateDate进化版
+        /// </summary>
+        /// <param name="fileInfos"></param>
+        /// <returns></returns>
+        private List<DateTime> GetFileUpdateDateList(List<FileInfo> fileInfos)
+        {
+            return fileInfos.ConvertAll<DateTime>(f => GetFileUpdateDate(f));
         }
     }
 }
